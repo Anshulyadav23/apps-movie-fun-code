bf4595c90988f267f61fa766cb3bbafb5e2286d3
Anshul <anshul7415@gmail.com>
2018-04-18 11:57:50 -0600

    Spring Bootification - Final Code

--- src/test/java/org/superbiz/moviefun/SmokeTest.java
+++ src/test/java/org/superbiz/moviefun/SmokeTest.java
@@ -11,12 +11,10 @@ public class SmokeTest {
     @Test
     public void smokeTest() {
         RestTemplate restTemplate = new RestTemplate();
-
         String homePage = restTemplate.getForObject(url("/"), String.class);
-
         assertThat(homePage, containsString("Please select one of the following links:"));
 
-        String setupPage = restTemplate.getForObject(url("/setup.jsp"), String.class);
+        String setupPage = restTemplate.getForObject(url("/setup"), String.class);
 
         assertThat(setupPage, containsString("Wedding Crashers"));
         assertThat(setupPage, containsString("Starsky & Hutch"));
@@ -33,11 +31,9 @@ public class SmokeTest {
     private String url(String path) {
         String baseUrl = "http://localhost:8080/moviefun";
         String envUrl = System.getenv("MOVIE_FUN_URL");
-
         if (envUrl != null && !envUrl.isEmpty()) {
             baseUrl = envUrl;
         }
-
         return baseUrl + path;
     }
 }
